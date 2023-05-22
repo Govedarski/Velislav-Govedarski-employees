ca8f54a228945ff1c2b5e120caef6c24e90cf6d0
Govedarski <velislavgovedarski@gmail.com>
2023-05-22 19:26:21 +0300

    Created ResultTable with hardcoded row.

---
+++ src/components/ResultTable/ResultTable.js
@@ -0,0 +1,24 @@
+export function ResultTable({data}) {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Employee ID #1</th>
+                        <th>Employee ID #2</th>
+                        <th>Project ID</th>
+                        <th>Days Worked</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>143</td>
+                        <td>218</td>
+                        <td>10</td>
+                        <td>8</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
